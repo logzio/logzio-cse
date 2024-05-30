f7623d443a7e80a329e17abb5f062cfbc970f01b
SMony-L <sovanmony.lim@logz.io>
2024-05-30 12:37:20 -0400

    Update deployment volumes and logs

--- fastapi-logzio-otel/main.py
+++ fastapi-logzio-otel/main.py
@@ -6,17 +6,29 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from datetime import datetime, timezone
+import json
+
+class CustomJSONFormatter(logging.Formatter):
+    def format(self, record):
+        log_entry = {
+            "log": f"{record.levelname}: {record.getMessage()}",
+            "stream": "stdout" if record.levelno < logging.ERROR else "stderr",
+            "time": datetime.fromtimestamp(record.created, timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        }
+        return json.dumps(log_entry)
 
 app = FastAPI()
 
 log_path = "/kapow/data/Logs/fastapi_app.log"
+file_handler = logging.FileHandler(log_path)
+file_handler.setFormatter(CustomJSONFormatter())
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(CustomJSONFormatter())
+
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler(log_path),
-        logging.StreamHandler()
-    ]
+    handlers=[file_handler, stream_handler]
 )
 logger = logging.getLogger(__name__)
 
